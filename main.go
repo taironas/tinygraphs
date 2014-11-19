0021589929efbab783f68e12fb46bc1c0dee072f
santiaago <santiago.ariassar@gmail.com>
2014-11-19 21:29:45 +0000

    change port

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":5000", nil)
-    if err != nil {
-      panic(err)
-    }
+	http.HandleFunc("/", hello)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, green-albatros")
+	fmt.Fprintln(res, "hello, green-albatros")
 }
