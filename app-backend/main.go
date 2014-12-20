334c773a876e810c0aaa1098edb671526ed18d10
santiaago <santiago.ariassar@gmail.com>
2014-12-20 20:25:32 +0000

    app now handles angular routes and golang routes

--- app-backend/main.go
+++ app-backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,10 +11,20 @@ import (
 var root = flag.String("root", "app", "file system path")
 
 func main() {
+	http.HandleFunc("/black/", blackHandler)
+	http.HandleFunc("/green/", greenHandler)
 	http.Handle("/", http.FileServer(http.Dir(*root)))
 	log.Println("Listening on " + os.Getenv("PORT"))
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+}
+
+func blackHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "black handler!")
+}
+
+func greenHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "green handler!")
 }
