9d3dae9a7cb34b1c8a004bbca2ce5e6812be4e2f
santiaago <santiago.ariassar@gmail.com>
2014-12-21 23:46:45 +0000

    add local anchors probably we will change this into scrollTo(something)

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    config(['$routeProvider', function($routeProvider) {
+	$routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
