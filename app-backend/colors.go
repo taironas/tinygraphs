bd5cf45de3fb845e8af999f21d33afca96561ce0
santiaago <santiago.ariassar@gmail.com>
2015-01-08 22:03:12 +0000

    refactor color pattern map

---
+++ app-backend/colors.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"image/color"
+)
+
+func MapOfColorPatterns() map[int][]color.RGBA {
+	return map[int][]color.RGBA{
+		0: []color.RGBA{
+			color.RGBA{uint8(255), uint8(255), uint8(255), uint8(255)},
+			color.RGBA{uint8(0), uint8(0), uint8(0), uint8(255)},
+		},
+
+		1: []color.RGBA{
+			color.RGBA{uint8(0), uint8(144), uint8(167), uint8(255)},
+			color.RGBA{uint8(0), uint8(214), uint8(244), uint8(255)},
+		},
+		2: []color.RGBA{
+			color.RGBA{uint8(232), uint8(70), 134, 255},
+			color.RGBA{uint8(181), uint8(181), 181, 255},
+		},
+		3: []color.RGBA{
+			color.RGBA{uint8(255), uint8(237), 81, 255},
+			color.RGBA{uint8(255), uint8(253), 136, 255},
+		},
+		4: []color.RGBA{
+			color.RGBA{uint8(177), uint8(192), 24, 255},
+			color.RGBA{uint8(86), uint8(165), 150, 255},
+		},
+		5: []color.RGBA{
+			color.RGBA{uint8(208), uint8(2), 120, 255},
+			color.RGBA{uint8(255), uint8(0), 118, 255},
+		},
+		6: []color.RGBA{
+			color.RGBA{uint8(0), uint8(44), 47, 255},
+			color.RGBA{uint8(126), uint8(176), 119, 255},
+		},
+		7: []color.RGBA{
+			color.RGBA{uint8(29), uint8(24), 18, 255},
+			color.RGBA{uint8(234), uint8(225), 219, 255},
+		},
+		8: []color.RGBA{
+			color.RGBA{uint8(33), uint8(30), 26, 255},
+			color.RGBA{uint8(176), uint8(209), 194, 255},
+		},
+	}
+}
--- app-backend/randomgrid.go
+++ app-backend/randomgrid.go
@@ -14,41 +14,8 @@ func randomColorGridHandler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("error when extracting permalink id: %v", err)
 	} else {
 		m := image.NewRGBA(image.Rect(0, 0, 240, 240))
-		var c1, c2 color.RGBA
-		if intID == 1 {
-			c1 = color.RGBA{uint8(0), uint8(144), 167, 255}
-			c2 = color.RGBA{uint8(0), uint8(214), 244, 255}
-		}
-		if intID == 2 {
-			c1 = color.RGBA{uint8(232), uint8(70), 134, 255}
-			c2 = color.RGBA{uint8(181), uint8(181), 181, 255}
-		}
-		if intID == 3 {
-			c1 = color.RGBA{uint8(255), uint8(237), 81, 255}
-			c2 = color.RGBA{uint8(255), uint8(253), 136, 255}
-		}
-		if intID == 4 {
-			c1 = color.RGBA{uint8(177), uint8(192), 24, 255}
-			c2 = color.RGBA{uint8(86), uint8(165), 150, 255}
-		}
-		if intID == 5 {
-			c1 = color.RGBA{uint8(208), uint8(2), 120, 255}
-			c2 = color.RGBA{uint8(255), uint8(0), 118, 255}
-		}
-		if intID == 6 {
-			c1 = color.RGBA{uint8(0), uint8(44), 47, 255}
-			c2 = color.RGBA{uint8(126), uint8(176), 119, 255}
-		}
-		if intID == 7 {
-			c1 = color.RGBA{uint8(29), uint8(24), 18, 255}
-			c2 = color.RGBA{uint8(234), uint8(225), 219, 255}
-		}
-		if intID == 8 {
-			c1 = color.RGBA{uint8(33), uint8(30), 26, 255}
-			c2 = color.RGBA{uint8(176), uint8(209), 194, 255}
-		}
-
-		drawRandomGrid6X6(m, c1, c2)
+		colorMap := MapOfColorPatterns()
+		drawRandomGrid6X6(m, colorMap[int(intID)][0], colorMap[int(intID)][1])
 		var img image.Image = m
 		writeImage(w, &img)
 	}
