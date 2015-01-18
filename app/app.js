7dd4a0846d28e55980bbca57eb47504f6b3c6e96
santiaago <santiago.ariassar@gmail.com>
2015-01-18 18:59:53 +0000

    change main route to / and fix links

--- app/app.js
+++ app/app.js
@@ -9,5 +9,5 @@ angular.module('tinygraphs', [
     'tinygraphs.holder'
 ]).
     config(['$routeProvider', function($routeProvider) {
-	$routeProvider.otherwise({redirectTo: '/view1'});
+	$routeProvider.otherwise({redirectTo: '/'});
     }]);
