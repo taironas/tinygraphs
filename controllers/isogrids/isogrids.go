615d1e4ce05d2a31e7fd38559122e5acd71f12f4
santiaago <santiago.ariassar@gmail.com>
2015-01-26 21:03:27 +0000

    add lab.go

--- controllers/isogrids/isogrids.go
+++ controllers/isogrids/isogrids.go
@@ -49,35 +49,3 @@ func Isogrids(w http.ResponseWriter, r *http.Request) {
 	write.ImageSVG(w)
 	draw.Isogrids(w, key, bg, fg, size)
 }
-
-func GridBW(w http.ResponseWriter, r *http.Request) {
-
-	colorMap := colors.MapOfColorPatterns()
-	var err error
-	var bg, fg color.RGBA
-	if bg, err = extract.Background(r); err != nil {
-		bg = colorMap[0][0]
-	}
-	if fg, err = extract.Foreground(r); err != nil {
-		fg = colorMap[0][1]
-	}
-	size := extract.Size(r)
-	write.ImageSVG(w)
-	draw.IsogridsBW(w, "", bg, fg, size)
-}
-
-func Grid2Colors(w http.ResponseWriter, r *http.Request) {
-
-	colorMap := colors.MapOfColorPatterns()
-	var err error
-	var bg, fg color.RGBA
-	if bg, err = extract.Background(r); err != nil {
-		bg = colorMap[0][0]
-	}
-	if fg, err = extract.Foreground(r); err != nil {
-		fg = colorMap[0][1]
-	}
-	size := extract.Size(r)
-	write.ImageSVG(w)
-	draw.Isogrids2Colors(w, "", bg, fg, size)
-}
