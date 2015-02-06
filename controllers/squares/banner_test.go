51cbd249d10a3df604ee0918a938ad506b75187b
santiaago <santiago.ariassar@gmail.com>
2015-02-06 20:23:30 +0000

    add controller banner test

---
+++ controllers/squares/banner_test.go
@@ -0,0 +1,30 @@
+package squares
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/taironas/route"
+	tgTesting "github.com/taironas/tinygraphs/testing"
+)
+
+func TestBannerRandom(t *testing.T) {
+
+	r := new(route.Router)
+	r.HandleFunc("/squares/banner/random", BannerRandom)
+
+	test := tgTesting.GenerateHandlerFunc(t, BannerRandom)
+	for _, p := range tgTesting.GoodParams {
+		recorder := test("/squares/banner/random", "GET", p, r)
+		if recorder.Code != http.StatusOK {
+			t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusOK)
+		}
+	}
+
+	for _, p := range tgTesting.BadParams {
+		recorder := test("/squares/banner/random", "GET", p, r)
+		if recorder.Code != http.StatusOK {
+			t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusOK)
+		}
+	}
+}
