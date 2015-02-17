fac22198c655e83b3702c854e484fdc3a1470926
santiaago <santiago.ariassar@gmail.com>
2015-02-17 19:49:23 +0000

    first draft of func Palette in jpeg format #31

--- draw/squares/palette.go
+++ draw/squares/palette.go
@@ -5,6 +5,15 @@ import (
 	"image/color"
 )
 
+// Palette builds an image with all the colors present in the theme array.
 func Palette(m *image.RGBA, theme []color.RGBA) {
-
+	size := m.Bounds().Size()
+	numColors := len(theme)
+	quadrant := size.X / numColors
+	for x := 0; x < size.X; x++ {
+		currentQuadrant := (x / quadrant) % numColors
+		for y := 0; y < size.Y; y++ {
+			m.Set(x, y, theme[currentQuadrant])
+		}
+	}
 }
