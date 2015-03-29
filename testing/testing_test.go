5bc854231c10c00eb3b538aabacc723484e7061a
santiaago <santiago.ariassar@gmail.com>
2015-03-29 19:42:30 +0100

    test testing pkg

---
+++ testing/testing_test.go
@@ -0,0 +1,26 @@
+package testing
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/taironas/route"
+)
+
+func TestGenerateHandlerFunc(t *testing.T) {
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello, world")
+	}
+
+	r := new(route.Router)
+	r.HandleFunc("/test", handler)
+
+	test := GenerateHandlerFunc(t, handler)
+
+	recorder := test("/test", "GET", map[string]string{}, r)
+	if recorder.Code != http.StatusOK {
+		t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusOK)
+	}
+}
