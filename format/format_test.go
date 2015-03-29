601e0b70ed583f9a3d8066c273c48f383ebadb23
santiaago <santiago.ariassar@gmail.com>
2015-03-29 22:38:15 +0100

    remove unnecessary test

--- format/format_test.go
+++ format/format_test.go
@@ -3,10 +3,4 @@ package format
 import "testing"
 
 func TestFormat(t *testing.T) {
-	if JPEG != 0 {
-		t.Errorf("Expected 0 got,", JPEG)
-	}
-	if SVG != 1 {
-		t.Errorf("Expected 1 got,", SVG)
-	}
 }
