d48b8003c7b2b90ef102af698eb3dbff0554cb48
santiaago <santiago.ariassar@gmail.com>
2015-03-30 20:27:44 +0100

    test palette.go

---
+++ draw/squares/palette_test.go
@@ -0,0 +1,21 @@
+package squares
+
+import (
+	"image"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPalette(t *testing.T) {
+	img := image.NewRGBA(image.Rect(0, 0, 10, 10))
+	Palette(img, colorTheme)
+}
+
+func TestPaletteSVG(t *testing.T) {
+	rec := httptest.NewRecorder()
+	PaletteSVG(rec, colorTheme, 100, 10)
+	if rec.Code != http.StatusOK {
+		t.Errorf("returned %v. Expected %v.", rec.Code, http.StatusOK)
+	}
+}
