421dc697a65e4ba9f6169dcd7cc096b55eeb171f
santiaago <santiago.ariassar@gmail.com>
2015-04-09 22:37:07 +0100

    test cache for space invader route.

--- controllers/spaceinvaders/spaceinvaders_test.go
+++ controllers/spaceinvaders/spaceinvaders_test.go
@@ -2,6 +2,7 @@ package spaceinvaders
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/taironas/route"
@@ -25,6 +26,33 @@ func TestSpaceInvaders(t *testing.T) {
 		recorder := test("/spaceinvaders/somekey", "GET", p, r)
 		if recorder.Code != http.StatusOK {
 			t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusOK)
+		}
+	}
+}
+
+func TestSpaceInvadersCache(t *testing.T) {
+	t.Parallel()
+	r := new(route.Router)
+	r.HandleFunc("/spaceinvaders/:key", SpaceInvaders)
+	var etag string
+	test := tgTesting.GenerateHandlerFunc(t, SpaceInvaders)
+
+	if recorder := test("/spaceinvaders/somekey", "GET", nil, r); recorder != nil {
+		if recorder.Code != http.StatusOK {
+			t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusOK)
+		}
+		etag = recorder.Header().Get("Etag")
+	}
+
+	// test caching
+	if req, err := http.NewRequest("GET", "/spaceinvaders/somekey", nil); err != nil {
+		t.Errorf("%v", err)
+	} else {
+		req.Header.Set("If-None-Match", etag)
+		recorder := httptest.NewRecorder()
+		r.ServeHTTP(recorder, req)
+		if recorder.Code != http.StatusNotModified {
+			t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusNotModified)
 		}
 	}
 }
